d432f66d19eb9eba4f60a1d10af8d60d4e503233
a19357 <a19357@d1-05-11.dped.local>
2018-02-06 15:20:28 +0000

    versão 1

---
+++ src/m17b_fichaaval/Ponto2D.java
@@ -0,0 +1,41 @@
+package m17b_fichaaval;
+
+public class Ponto2D {
+    private int x;
+    private int y;
+    
+    public Ponto2D(){
+        x = 0;
+        y = 0;        
+    }
+    
+    public Ponto2D(int x, int y){
+        this.x = x;
+        this.y = y;        
+    }
+    
+    public double distancia(Ponto2D a){
+        return Math.abs(Math.sqrt(Math.pow((x-a.x),2)+Math.pow((y-a.y),2)));
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Ponto2D{" + "x=" + x + ", y=" + y + '}';
+    }
+}
